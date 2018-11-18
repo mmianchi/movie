d0ccc7d0a56963ac38b4bd9ce0afa0a83f3e5e51
mmianchi <mmianchi@gmail.com>
2018-11-18 01:51:38 -0800

    update

--- src/App.js
+++ src/App.js
@@ -4,25 +4,33 @@ import './App.css';
 
 
 class App extends Component {
+
+  toggle = () => {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Welcome text="Welcome to React!5" />
+          <Welcome text="Welcome to React!!!" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-    );
+        
+
+        <button onClick={this.toggle}>Show / Hide</button>
+      </div>;
   }
 }
 
 class Welcome extends Component {
   render() {
+    const { text } = this.props;
     return (
-      <h1 className="App-title">{this.props.text}</h1>
+      <h1 className="App-title">{text}</h1>
     )
   }
 } 
